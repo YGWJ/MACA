aa787eb395d04ae80521f82d46a08bfa4576d8d1
GuiYaJun <329783568@qq.com>
2015-04-13 21:52:41 +0800

    dock

--- MACA/Main/MainViewController.m
+++ MACA/Main/MainViewController.m
@@ -103,9 +103,9 @@
 - (void) addDocks
 {
     //2.往dock里面填充内容
-    [self.dock addItemWithIcon:@"isBuy.png" andTitle:@"云医"];
-    [self.dock addItemWithIcon:@"search.png" andTitle:@"幼儿"];
-    [self.dock addItemWithIcon:@"person.png" andTitle:@"发现"];
+    [self.dock addItemWithIcon:@"home.png" andTitle:@"云医"];
+    [self.dock addItemWithIcon:@"baby.png" andTitle:@"幼儿"];
+    [self.dock addItemWithIcon:@"discover.png" andTitle:@"发现"];
     [self.dock addItemWithIcon:@"person.png" andTitle:@"个人"];
     UIView *viewLine = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 1)];
     [viewLine setBackgroundColor:[Tools colorWithRed:221 angGreen:221 andBlue:221]];
